fd1dd376e7e2728d5d981fb49f9279374d587121
takahashi1682 <h.takahashi@morijyobi.ac.jp>
2024-10-03 14:21:11 +0900

    問5追加

---
+++ Assets/Projects/99_Challenge/チャレンジ問題5/Challenge.cs
@@ -0,0 +1,20 @@
+using System;
+using Projects.Viewer;
+using R3;
+using UnityEngine;
+
+namespace Projects._99_Challenge.チャレンジ問題5
+{
+    public class Challenge : MonoBehaviour, ITextBinder
+    {
+        private readonly ReactiveProperty<int> _count = new(1000);
+
+        // UI表示用
+        public ReadOnlyReactiveProperty<string> Text => _count.Select(x => x.ToString()).ToReadOnlyReactiveProperty();
+
+        private void Awake()
+        {
+            _count.AddTo(this);
+        }
+    }
+}
