4853e59c633fcf1b40665e4c93ec73ac39c90dcd
takahashi1682 <h.takahashi@morijyobi.ac.jp>
2024-11-07 12:00:39 +0900

    課題修正

--- Assets/Projects/30_UniTask/30_8_Forgetで非同期処理を実行する/MyTask.cs
+++ Assets/Projects/30_UniTask/30_8_Forgetで非同期処理を実行する/MyTask.cs
@@ -14,11 +14,15 @@ namespace Projects._30_UniTask._30_8_Forgetで非同期処理を実行する
             var token = destroyCancellationToken;
 
             // 一定時間後にログを出力する非同期処理を実行する
-            DelayLog(5, "5秒経過しました", token).Forget();
-            DelayLog(3, "3秒経過しました", token).Forget();
-            DelayLog(1, "1秒経過しました", token).Forget();
-            
-            Debug.Log("スタート");
+            DelayLog(5, "5秒間の処理", token).Forget(); // 待たずに次へ
+            DelayLog(3, "3秒間の処理", token).Forget(); // 待たずに次へ
+            DelayLog(1, "1秒間の処理", token).Forget(); // 待たずに次へ
+
+            Debug.Log("この時点で0秒かかる");
+
+            // 1秒待機する
+            // 3秒待機する
+            // 5秒待機する
         }
 
         private async UniTask DelayLog(float second, string message, CancellationToken token)
--- Assets/Projects/99_Challenge/チャレンジ問題30/BomController.cs
+++ Assets/Projects/99_Challenge/チャレンジ問題30/BomController.cs
@@ -4,8 +4,9 @@ namespace Projects._99_Challenge.チャレンジ問題30
 {
     public class BomController : MonoBehaviour
     {
-        [SerializeField] private float _explosionTime = 3f;
+        [SerializeField] private SpriteRenderer _target;
         [SerializeField] private Sprite _explosionSprite;
+        [SerializeField] private float _explosionTime = 3f;
 
         /// <summary>
         /// Startメソッドを書き換えてください
